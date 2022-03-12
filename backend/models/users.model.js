aaf777a7ac2252fd8229c83a6d8fc83f3f18ce14
Samaresh Samanta <ss9261@srmist.edu.in>
2022-03-12 11:39:19 +0530

    add: backend and frontend

---
+++ backend/models/users.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        minlength: 1        
+    },
+    password: {
+        type:String,
+        required:true,      
+    },
+    email: {
+        type:String,
+        required:true      
+    },
+},{
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ backend/routes/task.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+let Task = require('../models/tasks.model');
+
+router.route('/').get((req,res)=>{
+    Task.find()
+        .then(task=>{
+            res.json(task);
+        })
+        .catch(err=>res.status(400).json(err))
+})
+
+router.route('/add/').post(async (req,res)=>{
+    const { userId,task } = req.body;
+
+    const existingTasks = await Task.findOne({ userId });
+
+    var updatedTasks = existingTasks.task;
+
+    const new_task = {
+        task,
+        status:"not done"
+    }
+
+    updatedTasks.push(new_task)
+
+    Task.replaceOne({userId:userId}, { userId,task:updatedTasks })
+        .then(()=>{res.json('task added!')})
+        .catch(err=>res.status(400).json(err))
+})
+
+
+router.route('/edit/').post(async (req,res)=>{
+    const { taskId, method, userId } = req.body;
+
+    const existingTasks = await Task.findOne({ userId });
+
+    var updatedTasks = [];
+    if(method === "delete"){
+        if(existingTasks){
+            existingTasks.task.map(t=>{
+                if(JSON.stringify(t._id).replace('"','').replace('"','') !== taskId){
+                    updatedTasks.push(t);
+                }
+            })
+        }
+    }
+    else if(method === "update"){
+        if(existingTasks){
+            existingTasks.task.map(t=>{
+                if(JSON.stringify(t._id).replace('"','').replace('"','') === taskId){
+                    if(t.status === 'done'){
+                        t.status = 'not done';
+                    }else{
+                        t.status = 'done';
+                    }
+                    updatedTasks.push(t);
+                }else{
+                    updatedTasks.push(t);
+                }
+            })
+        }
+    }
+
+    Task.replaceOne({userId:userId}, { userId,task:updatedTasks })
+        .then(()=>{res.json("task updated!")})
+        .catch(err=>res.status(400).json(err))
+})
+
+module.exports = router;
